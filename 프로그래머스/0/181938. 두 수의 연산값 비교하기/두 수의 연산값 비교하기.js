f7b23c5101bd0ab880263f3ac3b5ef9f0a6025b9
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-09-06 10:29:12 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+    var answer = 0;
+    const num1 = Number(String(a) + String(b));
+    const num2 = 2 * a * b;
+    answer = num1 >= num2 ? num1 : num2;
+    return answer;
+}
