4ca231b8cfc29f12e7cb6832161b38af5eefba57
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2024-10-06 19:52:38 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 49.07 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    const answer = arr.map(el => {
+        if (el >= 50 && el % 2 === 0) {
+            return el / 2;
+        } else if (el < 50 && el % 2 === 1) {
+            return el * 2;
+        } else {
+            return el;
+        }
+    })
+    return answer;
+}
