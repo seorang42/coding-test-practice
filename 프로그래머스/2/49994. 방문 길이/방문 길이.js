ca00876ea62ac079772929a0ba490946bfe1216e
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-02-28 20:55:31 +0900

    [level 2] Title: 방문 길이, Time: 1.30 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,44 +1,36 @@
+function isValid(nx, ny) {
+    return nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5;
+}
+
+function updateLocation(x, y, dir) {
+    switch (dir) {
+        case "U":
+            return [x, y + 1];
+        case "D":
+            return [x, y - 1];
+        case "R":
+            return [x + 1, y];
+        case "L":
+            return [x - 1, y];
+    }
+}
+
 function solution(dirs) {
-    const xField = new Array(11).fill(0).map(el => new Array(11).fill(0));
-    const yField = new Array(11).fill(0).map(el => new Array(11).fill(0));
-    const [dx, dy] = [[0, 0, 1, -1], [1, -1, 0, 0]];
-    const dir = {U : 0, D : 1, R : 2, L : 3};
-    let [x, y] = [5, 5];
+    let [x, y] = [0, 0];
     
-    let answer = 0;
-    for (let i = 0; i < dirs.length; i++) {
-        if (dirs[i] === "U" || dirs[i] === "D") {
-            if (y + dy[dir[dirs[i]]] < 0 || y + dy[dir[dirs[i]]] > 10) {
-                continue;
-            }
-            if (dirs[i] === "U") {
-                y += dy[dir[dirs[i]]];
-            }
-            if (yField[y][x] === 0) {
-                answer++;
-                yField[y][x]++;
-            }
-            if (dirs[i] === "D") {
-                y += dy[dir[dirs[i]]];
-            }
+    const visited = new Set();
+    for (const dir of dirs) {
+        const [nx, ny] = updateLocation(x, y, dir);
+        
+        if (!isValid(nx, ny)) {
+            continue;
         }
         
-        if (dirs[i] === "R" || dirs[i] === "L") {
-            if (x + dx[dir[dirs[i]]] < 0 || x + dx[dir[dirs[i]]] > 10) {
-                continue;
-            }
-            if (dirs[i] === "L") {
-                x += dx[dir[dirs[i]]];
-            }
-            if (xField[y][x] === 0) {
-                answer++;
-                xField[y][x]++;
-            }
-            if (dirs[i] === "R") {
-                x += dx[dir[dirs[i]]];
-            }
-        }
+        visited.add(`${x}${y}${nx}${ny}`);
+        visited.add(`${nx}${ny}${x}${y}`);
+        
+        [x, y] = [nx, ny];
     }
     
-    return answer;
+    return visited.size / 2;
 }
