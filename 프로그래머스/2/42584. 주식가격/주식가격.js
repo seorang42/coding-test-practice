0af94813e42623000520ec5e8a662c64d1ed2a14
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-03-02 00:47:59 +0900

    [level 2] Title: 주식가격, Time: 29.25 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,14 +1,20 @@
 function solution(prices) {
-    const answer = new Array(prices.length).fill(0);
-    for (let i = 0; i < prices.length - 1; i++) {
-        let count = 0;
-        for (let j = i + 1; j < prices.length; j++) {
-            count++;
-            if (prices[i] > prices[j]) {
-                break;
-            }
+    const n = prices.length;
+    const answer = new Array(n).fill(0);
+    
+    const stack = [0];
+    for (let i = 1; i < n; i++) {
+        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+            const j = stack.pop();
+            answer[j] = i - j;
         }
-        answer[i] = count;
+        stack.push(i);
     }
+    
+    while (stack.length > 0) {
+        const j = stack.pop();
+        answer[j] = n - 1 - j;
+    }
+    
     return answer;
 }
