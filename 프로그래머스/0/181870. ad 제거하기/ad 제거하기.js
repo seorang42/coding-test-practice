5cf405dda7e72e5903c368d0fb2627323c8e12a6
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-03-14 22:53:21 +0900

    [level 0] Title: ad 제거하기, Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -0,0 +1,9 @@
+function solution(strArr) {
+    var answer = [];
+    for (const str of strArr) {
+        if (!str.includes('ad')) {
+            answer.push(str);
+        }
+    }
+    return answer;
+}
