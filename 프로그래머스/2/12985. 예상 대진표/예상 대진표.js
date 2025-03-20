0658ebb3918a136656b39d48d7660c57e75022e6
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2025-03-20 19:35:27 +0900

    [level 2] Title: 예상 대진표, Time: 0.10 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,15 @@
+function calcNum(num) {
+    if (num % 2 === 0) return num / 2;
+    else return (num + 1) / 2;
+}
+
 function solution(n,a,b) {
-    let answer = 0;
-    while (a !== b) {
-        a = Math.ceil(a / 2);
-        b = Math.ceil(b / 2);
+    let [A, B] = [a, b]
+    let answer = 1;
+    while (!(Math.abs(A - B) === 1 && Math.min(A, B) % 2 === 1)) {
         answer++;
+        A = calcNum(A);
+        B = calcNum(B);
     }
     
     return answer;
