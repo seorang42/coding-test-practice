1eedd9a1952dba0e96810ee052a578ace3dbdc12
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2026-02-09 23:15:44 +0900

    [Bronze V] Title: 사칙연산, Time: 92 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const [a, b] = fs.readFileSync("/dev/stdin").toString().split(" ").map(el => Number(el));
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
