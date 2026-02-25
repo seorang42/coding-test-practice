b35cbfc75c374d32029d2ed2fa762222b6862b36
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2026-02-26 00:51:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,30 +1,13 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim();
 
-const numbers = [];
-const operators = [];
+const groups = input.split("-");
+const numbers = groups.map((group) => group.split("+").map(Number).reduce((acc, cur) => acc + cur, 0));
 
-let lastIndex = 0;
-for (let i = 0; i < input.length; i++) {
-    if (isNaN(input[i])) {
-        const number = Number(input.slice(lastIndex, i));
-        numbers.push(number);
-        operators.push(input[i]);
-        lastIndex = i + 1;
-    }
-    
-    if (i === input.length - 1) {
-        const number = Number(input.slice(lastIndex));
-        numbers.push(number);
-    }
-}
+let answer = 0;
+numbers.forEach((number, index) => {
+    if (index === 0) answer += number;
+    else answer -= number;
+})
 
-const minusIndex = operators.indexOf("-");
-
-if (minusIndex === -1) {
-    console.log(numbers.reduce((acc, cur) => acc + cur, 0));
-} else {
-    const prevMinus = numbers.slice(0, minusIndex + 1).reduce((acc, cur) => acc + cur, 0);
-    const afterMinus = numbers.slice(minusIndex + 1).reduce((acc, cur) => acc + cur, 0);
-    console.log(prevMinus - afterMinus);
-}
+console.log(answer);
