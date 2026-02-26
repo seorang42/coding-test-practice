894ccd178a21b740a37de8ee58bd8553a057662e
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2026-02-26 18:05:38 +0900

    [Silver III] Title: 주유소, Time: 288 ms, Memory: 43936 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,18 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const [n, distances, costs] = [Number(input[0]), input[1].split(" ").map(Number), input[2].split(" ").map(Number)];
+const [n, distances, costs] = [Number(input[0]), input[1].split(" ").map(BigInt), input[2].split(" ").map(BigInt)];
 
-let cost = 0;
-for (let i = 0; i < n - 1; i++) {
-    cost += costs[i] * distances[i];
-    
-    let next = i + 1;
-    while (costs[i] < costs[next] && next < n - 1) {
-        cost += costs[i] * distances[next];
-        next++;
-    }
-    
-    if (next !== i + 1) i = next;
+let [cost, minCost] = [0n, Infinity];
+for (let i = 0; i < distances.length; i++) {
+    if (costs[i] < minCost) minCost = costs[i];
+    cost += distances[i] * minCost;
 }
 
-console.log(cost);
+console.log(cost.toString());
