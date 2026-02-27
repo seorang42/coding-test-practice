dba0ba399ce461f06035fc9a51bcf7a2f8860d8a
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2026-02-27 18:43:50 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1196 ms, Memory: 208092 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -2,34 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const [n, cards, m, targets] = [Number(input[0]), input[1].split(" ").map(Number).sort((a, b) => a - b), Number(input[2]), input[3].split(" ").map(Number)];
 
-const lowerbound = (arr, target, start, end) => {
-    while (start < end) {
-        const mid = parseInt((start + end) / 2);
-        if (arr[mid] >= target) {
-            end = mid;
-        } else {
-            start = mid + 1;
-        }
-    }
-    return end;
-}
+const numMap = {};
+cards.forEach((card) => {numMap[card] = (numMap[card] || 0) + 1});
 
-const upperbound = (arr, target, start, end) => {
-    while (start < end) {
-        const mid = parseInt((start + end) / 2);
-        if (arr[mid] <= target) {
-            start = mid + 1;
-        } else {
-            end = mid
-        }
-    }
-    return end;
-}
-
-const getCount = (arr, target) => {
-    const start = lowerbound(arr, target, 0, arr.length);
-    const end = upperbound(arr, target, 0, arr.length);
-    return end - start;
-}
-
-console.log(targets.map(target => getCount(cards, target)).join(" "));
+console.log(targets.map(target => (numMap[target] || 0)).join(" "));
