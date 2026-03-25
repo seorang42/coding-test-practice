ef5dc07196c1874b70e8549cfd096f79386895aa
Jangho Yoon <123955813+seorang42@users.noreply.github.com>
2026-03-25 16:32:16 +0900

    [Silver II] Title: 병사 배치하기, Time: 164 ms, Memory: 9728 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
@@ -1,30 +1,18 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const [n, soldiers] = [Number(input[0]), input[1].split(" ").map(Number).reverse()];
 
-const lowerbound = (arr, target, start, end) => {
-    while (start < end) {
-        const mid = parseInt((start + end) / 2);
-        if (arr[mid] >= target) {
-            end = mid;
-        } else {
-            start = mid + 1;
+const n = Number(input[0]);
+const soldiers = input[1].split(" ").map(Number);
+
+const dp = new Array(n).fill(1);
+
+for (let i = 0; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (soldiers[j] > soldiers[i]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
         }
     }
-    return end;
 }
 
-const getLIS = (arr) => {
-    const lis = [0];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > lis[lis.length - 1]) {
-            lis.push(arr[i]);
-        } else {
-            const index = lowerbound(lis, arr[i], 0, lis.length);
-            lis[index] = arr[i];
-        }
-    }
-    return lis;
-}
-
-console.log(soldiers.length - (getLIS(soldiers).length - 1));
+const max = Math.max(...dp);
+console.log(n - max);
